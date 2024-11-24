b97dce84256153adf1a5db2cccd3ca9d8ddc126c
awwwdev <->
2024-11-24 19:49:39 +0330

    fix: migrator

--- db/migrator.ts
+++ db/migrator.ts
@@ -1,18 +1,23 @@
 import 'dotenv/config';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
-import getDBClient from './client';
+// import getDBClient from './client';
 import config from "@/config";
+import { drizzle } from "drizzle-orm/postgres-js";
+// import * as schema from '../db/schema';
 
 
 
-const migrationConnection = postgres(config().POSTGRESQL_CONNECTION_STRING, { max: 1 });
 
-export default async function migrator(connectionString?: string) {
-  const dbClient = getDBClient(connectionString);
+
+export default async function migrator() {
+  const migrationConnection = postgres(config().POSTGRESQL_CONNECTION_STRING, { max: 1 });
+  // const dbClient = getDBClient(connectionString);
+  const dbClient = drizzle(migrationConnection);
   // This will run migrations on the database, skipping the ones already applied
-  await migrate(dbClient, { migrationsFolder: './drizzle' });
+  await migrate(dbClient, { migrationsFolder: "./drizzle" });
 
   // Don't forget to close the connection, otherwise the script will hang
   await migrationConnection.end();
 }
+
