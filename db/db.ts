71dd98950bfe5d8b786b3724b8d8ab4d70bd4820
awwwdev <->
2024-11-29 16:31:06 +0330

    fix: connect supabase and drizzle client

--- db/db.ts
+++ db/db.ts
@@ -1,36 +1,72 @@
 import config from "@/config";
-import { DrizzleConfig } from "drizzle-orm";
+import { sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-import { createClient } from "@/utils/supabase/server";
+// import { sql } from "drizzle-orm"
+import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
+// import postgres from "postgres"
 
-import * as schema from "../schema/schema";
+import { createClientForServer } from "@/utils/supabase/server";
+import schema from "../schema";
 import { decode } from "./jwt";
-import { createDrizzle } from "./supabaseDrizzle";
-
-const drizzleConfig = {
-  casing: "snake_case",
-  schema,
-} satisfies DrizzleConfig<typeof schema>;
 
 // ByPass RLS
 const admin = drizzle({
   client: postgres(config().ADMIN_POSTGRESQL_CONNECTION_STRING, { prepare: false }),
-  ...drizzleConfig,
+  // casing: "snake_case",
+  schema,
 });
 
 // Protected by RLS
 const client = drizzle({
   client: postgres(config().POSTGRESQL_CONNECTION_STRING, { prepare: false }),
-  ...drizzleConfig,
+  schema,
 });
+
+type SupabaseToken = {
+  iss?: string;
+  sub?: string;
+  aud?: string[] | string;
+  exp?: number;
+  nbf?: number;
+  iat?: number;
+  jti?: string;
+  role?: string;
+};
+
+type QueryInTransaction<T> = (tx: PostgresJsDatabase<typeof schema>) => Promise<T> | T;
 
 // https://github.com/orgs/supabase/discussions/23224
 // Should be secure because we use the access token that is signed, and not the data read directly from the storage
 export async function createDrizzleSupabaseClient() {
   const {
     data: { session },
-  } = await createClient().auth.getSession();
-  return createDrizzle(decode(session?.access_token ?? ""), { admin, client });
+  } = await createClientForServer().auth.getSession();
+  const token = decode(session?.access_token ?? "") as SupabaseToken;
+
+  return async <T>(callback: QueryInTransaction<T>) => {
+    return await client.transaction(async (tx) => {
+      // Supabase exposes auth.uid() and auth.jwt()
+      // https://supabase.com/docs/guides/database/postgres/row-level-security#helper-functions
+      try {
+        await tx.execute(sql`
+            -- auth.jwt()
+            select set_config('request.jwt.claims', '${sql.raw(JSON.stringify(token))}', TRUE);
+            -- auth.uid()
+            select set_config('request.jwt.claim.sub', '${sql.raw(token.sub ?? "")}', TRUE);
+            -- set local role
+            set local role ${sql.raw(token.role ?? "anon")};
+            `);
+        return await callback(tx);
+      } finally {
+        await tx.execute(sql`
+              -- reset
+              select set_config('request.jwt.claims', NULL, TRUE);
+              select set_config('request.jwt.claim.sub', NULL, TRUE);
+              reset role;
+              `);
+      }
+    });
+  };
 }
--- db/supabaseDrizzle.ts
+++ db/supabaseDrizzle.ts
@@ -1,5 +1,7 @@
-import { sql } from "drizzle-orm";
-import { PgDatabase } from "drizzle-orm/pg-core";
+import { ExtractTablesWithRelations, sql } from "drizzle-orm";
+import { PgDatabase, PgTransaction } from "drizzle-orm/pg-core";
+import { PostgresJsQueryResultHKT } from "drizzle-orm/postgres-js";
+import * as schema from "../schema/schema";
 
 type SupabaseToken = {
   iss?: string;
@@ -12,36 +14,43 @@ type SupabaseToken = {
   role?: string;
 };
 
-export function createDrizzle(token: SupabaseToken, { admin, client }: { admin: PgDatabase<any>; client: PgDatabase<any> }) {
-  return {
-    admin,
-    rls: (async (transaction, ...rest) => {
-      return await client.transaction(async (tx) => {
-        // Supabase exposes auth.uid() and auth.jwt()
-        // https://supabase.com/docs/guides/database/postgres/row-level-security#helper-functions
-        try {
-          await tx.execute(sql`
+type TX = PgTransaction<PostgresJsQueryResultHKT, typeof schema, ExtractTablesWithRelations<typeof schema>>;
+
+
+type QueryInTransaction<T> = (
+  tx: PgTransaction<
+    PostgresJsQueryResultHKT,
+    Record<string, never>,
+    ExtractTablesWithRelations<Record<string, never>>
+  >
+) => Promise<T>;
+
+export function createDrizzle<T>(
+  token: SupabaseToken,
+  { client }: { client: PgDatabase<any> }
+) {
+  return async (callback: QueryInTransaction<T>) => {
+    return await client.transaction(async (tx) => {
+      // Supabase exposes auth.uid() and auth.jwt()
+      // https://supabase.com/docs/guides/database/postgres/row-level-security#helper-functions
+      try {
+        await tx.execute(sql`
           -- auth.jwt()
-          select set_config('request.jwt.claims', '${sql.raw(
-            JSON.stringify(token)
-          )}', TRUE);
+          select set_config('request.jwt.claims', '${sql.raw(JSON.stringify(token))}', TRUE);
           -- auth.uid()
-          select set_config('request.jwt.claim.sub', '${sql.raw(
-            token.sub ?? ""
-          )}', TRUE);												
+          select set_config('request.jwt.claim.sub', '${sql.raw(token.sub ?? "")}', TRUE);
           -- set local role
           set local role ${sql.raw(token.role ?? "anon")};
           `);
-          return await transaction(tx);
-        } finally {
-          await tx.execute(sql`
+        return await callback(tx);
+      } finally {
+        await tx.execute(sql`
             -- reset
             select set_config('request.jwt.claims', NULL, TRUE);
             select set_config('request.jwt.claim.sub', NULL, TRUE);
             reset role;
             `);
-        }
-      }, ...rest);
-    }) as typeof client.transaction,
+      }
+    });
   };
 }
